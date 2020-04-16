9e266a3487590e22dd7da2ae04f1bfa2c724f4b3
Kislicin Alexey <a42phantom@gmail.com>
2020-04-16 10:48:01 +0300

    Add test class 1

--- src/Api/Controllers/Test.php
+++ src/Api/Controllers/Test.php
@@ -4,9 +4,10 @@
 namespace Hack2020\Api\Controllers;
 
 
+use Hack2020\Api\Controller;
 use Hack2020\Api\Response;
 
-class Test
+class Test extends Controller
 {
     public function GetTest() {
         Response::ShowResult(['test' => 'success']);
